bfa334759a0d17875d5caf2c00bafb1a4dae30d6
ankitgupta <mailtoankitgupta@gmail.com>
2017-03-12 15:18:57 +0530

    Updated HomeView with the framework

--- home/views/home.view.js
+++ home/views/home.view.js
@@ -1,18 +1,9 @@
 "use strict";
 (function($) {
-  function HomeView(model) {
-    this.root = $("<div></div>");
-
-    this.model = model;
-    this.initialize = function() {
-
-    };
-
-    this.render = function() {
+  window.HomeView = View.extend({
+    render: function() {
       this.root.html($("#homeTemplate").html());
       return this;
-    };
-  }
-
-  window.HomeView = HomeView;
+    }
+  });
 }) (jQuery);
